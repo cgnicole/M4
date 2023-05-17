7bc006701e50251ecc46df3162fd8c6b113a26af
cgnicole <soficamargo2004@gmail.com>
2023-05-16 20:50:10 -0500

    first commit

---
+++ 01-dbms/notas.js
@@ -0,0 +1,23 @@
+// sistema de gestion de base de datos (DBMS)
+
+/*
+
+Aprender a pensar que estructura va a tener nuestra base de datos 
+
+* las datospor si solos sin una estructura no significan nada, imaginemos una tabla exel con nuestro nombre en una cuadricula y nuestro apellido en otra, asi sueltos son datos pero necesitan una estructura definida para ser una base de datos
+
+
+Datos: son informacio o hechos que se pueden recopilar, estos datos se almacenan para tomar decicones informadas y obtener informacion util 
+
+Base de datos: es una coleccion organizada de datos que se alamacenan en una computadora y se puede acceder y utulizar facilmente 
+*/
+
+/* Modelo intedidad relacion:
+ 
+ entidad: representa una cosa, objeto o concepto del mundo real con existencia independiente.
+ atributos: los atributos son las caracteristicas que definen o indetifican una entidad.
+ relacion: es una asociacion entre varias entidades 
+
+  
+ 
+ */
